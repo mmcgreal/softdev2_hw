9e00982cbb159a500afeeb28acff9c747ac36d10
mmcgreal <mmcgreal14@yahoo.com>
2016-02-23 11:19:49 -0500

    no more sped up growth

--- hw02/animate.js
+++ hw02/animate.js
@@ -13,9 +13,14 @@ var radius = 0;
 var growing = true;
 var startDraw = document.getElementById("start");
 var endDraw = document.getElementById("stop");
+var requestID;
+var counter =0; //prevents speeding up
 
 function drawDot() {
-
+	counter +=1;
+	if (counter >1) //requestAnimationFrame can now only be called once- prevents speeding up.
+		  window.cancelAnimationFrame(requestID);
+	
   ctx.clearRect(0,0,c.width,c.height);
   ctx.strokeRect(0,0,c.width,c.height);
 
@@ -32,18 +37,20 @@ function drawDot() {
 		growing=true;
 	}
 	ctx.beginPath();
+	ctx.fillStyle = "#6600cc";
 	ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
 	ctx.stroke();
 	ctx.fill();
 
-  window.requestAnimationFrame(drawDot);
+  requestID = window.requestAnimationFrame(drawDot);
 };
 
 function stop() {
   ctx.clearRect(0,0,c.width,c.height); //clear canvas
-  growing = false;
+  growing = true;
+  counter=0;
   radius = 0;
-  window.cancelAnimationFrame(window.requestAnimationFrame(animate) );
+  window.cancelAnimationFrame(requestID);
 };
 
 startDraw.addEventListener("click", drawDot);
