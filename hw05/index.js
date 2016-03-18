00d2185b9258c010c1ff9fba10bf3f0e8516b49a
mmcgreal <mmcgreal14@yahoo.com>
2016-03-18 09:17:55 -0400

    js working

---
+++ hw05/index.js
@@ -0,0 +1,14 @@
+/*
+Mary McGreal
+SoftDev pd 3
+HW02- Animation
+2016-02-23
+*/
+var data = [4, 8, 15, 16, 23, 42];
+  d3.select(".chart")
+   .selectAll("div")
+    .data(data)
+    .enter().append("div")
+      .style("width", function(d){
+        return d*10+"px";})
+      .text(function(d) {return d;});
